b30b8f19f835e2ca3540d92d5be64b84a28f6f0d
aabidkhan0786 <62328527+aabidkhan0786@users.noreply.github.com>
2020-09-04 21:12:57 +0530

    Updates

---
+++ precache-manifest.54413ba86d0844146f1569e03e9cd8cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0f6611ec36e48269372f7c235ca96d",
+    "url": "/react_aCooks/index.html"
+  },
+  {
+    "revision": "adbce335e8f1fccbef97",
+    "url": "/react_aCooks/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dc39882e21c8479abbe2",
+    "url": "/react_aCooks/static/css/main.1ff91d9b.chunk.css"
+  },
+  {
+    "revision": "adbce335e8f1fccbef97",
+    "url": "/react_aCooks/static/js/2.4374103c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_aCooks/static/js/2.4374103c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc39882e21c8479abbe2",
+    "url": "/react_aCooks/static/js/main.2c84638a.chunk.js"
+  },
+  {
+    "revision": "1feb94d1c5654cdb2a6c",
+    "url": "/react_aCooks/static/js/runtime-main.739d2f98.js"
+  },
+  {
+    "revision": "18699e6cc63b48b3b5bc407996708867",
+    "url": "/react_aCooks/static/media/banner3.18699e6c.png"
+  },
+  {
+    "revision": "979515a5f1e0aa3f7e6545c7056bebbf",
+    "url": "/react_aCooks/static/media/food.979515a5.gif"
+  }
+]);
